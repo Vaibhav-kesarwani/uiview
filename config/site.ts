8c523c11ecfb61d2592c35e0a1d83d53c2dfd21c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-03-23 15:04:26 +0530

    Make the config of the site for the metadata

---
+++ config/site.ts
@@ -0,0 +1,18 @@
+export const siteConfig = {
+  name: "vector/ui",
+  url: "example URL",
+  ogImage: "Website Image",
+  description:
+    "A set of beautifully-designed, accessible components and a code distribution platform. Works with your favorite frameworks. Open Source. Open Code.",
+  links: {
+    twitter: "https://twitter.com/Vaibhav_k__",
+    github: "https://github.com/Vaibhav-kesarwani",
+  },
+};
+
+export type SiteConfig = typeof siteConfig;
+
+export const META_THEME_COLORS = {
+  light: "#ffffff",
+  dark: "#09090b",
+};
