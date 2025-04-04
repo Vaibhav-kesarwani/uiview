eba0a966b64e2db0e96cf50ef8c5d777dc571f1f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-04-04 22:02:34 +0530

    reset org

--- apps/docs/components/themes/templates/heroui.ts
+++
@@ -1,9 +0,0 @@
-import {initialDarkTheme, initialLayout, initialLightTheme} from "../constants";
-import {Config} from "../types";
-
-export const heroui: Config = {
-  name: "heroui",
-  light: initialLightTheme,
-  dark: initialDarkTheme,
-  layout: initialLayout,
-};
