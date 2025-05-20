73a162f4d79a700c6b44a7463a5ed10956505d1d
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-20 10:22:50 +0530

    Feat: added the marketing slug

---
+++ .content-collections/generated/allPages.js
@@ -0,0 +1,56 @@
+export default [
+  {
+    "content": "<ComponentPreview name=\"bento-demo\" preview />\n<ComponentPreview name=\"marquee-demo\" preview />\n<ComponentPreview name=\"marquee-demo-vertical\" preview />\n<ComponentPreview name=\"animated-list-demo\" preview />\n<ComponentPreview name=\"animated-beam-demo\" preview />\n<ComponentPreview name=\"animated-beam-multiple-inputs\" preview />\n<ComponentPreview name=\"animated-beam-multiple-outputs\" preview />\n<ComponentPreview name=\"border-beam-demo\" preview />\n<ComponentPreview name=\"border-beam-demo-2\" preview />\n<ComponentPreview name=\"border-beam-demo-3\" preview />\n<ComponentPreview name=\"border-beam-demo-4\" preview />\n<ComponentPreview name=\"shine-border-demo\" preview />\n<ComponentPreview name=\"magic-card-demo\" preview />\n<ComponentPreview name=\"dock-demo\" preview />\n<ComponentPreview name=\"particles-demo\" preview />\n<ComponentPreview name=\"blur-fade-demo\" preview />\n<ComponentPreview name=\"blur-fade-text-demo\" preview />\n<ComponentPreview name=\"retro-grid-demo\" preview />\n<ComponentPreview name=\"orbiting-circles-demo\" preview />\n<ComponentPreview name=\"ripple-demo\" preview />\n<ComponentPreview name=\"confetti-demo\" preview />",
+    "title": "Components",
+    "description": "A collection of components that are used to build the landing page of a website.",
+    "_meta": {
+      "filePath": "home.mdx",
+      "fileName": "home.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "home"
+    },
+    "slug": "/home",
+    "slugAsParams": "home",
+    "body": {
+      "raw": "<ComponentPreview name=\"bento-demo\" preview />\n<ComponentPreview name=\"marquee-demo\" preview />\n<ComponentPreview name=\"marquee-demo-vertical\" preview />\n<ComponentPreview name=\"animated-list-demo\" preview />\n<ComponentPreview name=\"animated-beam-demo\" preview />\n<ComponentPreview name=\"animated-beam-multiple-inputs\" preview />\n<ComponentPreview name=\"animated-beam-multiple-outputs\" preview />\n<ComponentPreview name=\"border-beam-demo\" preview />\n<ComponentPreview name=\"border-beam-demo-2\" preview />\n<ComponentPreview name=\"border-beam-demo-3\" preview />\n<ComponentPreview name=\"border-beam-demo-4\" preview />\n<ComponentPreview name=\"shine-border-demo\" preview />\n<ComponentPreview name=\"magic-card-demo\" preview />\n<ComponentPreview name=\"dock-demo\" preview />\n<ComponentPreview name=\"particles-demo\" preview />\n<ComponentPreview name=\"blur-fade-demo\" preview />\n<ComponentPreview name=\"blur-fade-text-demo\" preview />\n<ComponentPreview name=\"retro-grid-demo\" preview />\n<ComponentPreview name=\"orbiting-circles-demo\" preview />\n<ComponentPreview name=\"ripple-demo\" preview />\n<ComponentPreview name=\"confetti-demo\" preview />",
+      "code": "var Component=(()=>{var v=Object.create;var o=Object.defineProperty;var c=Object.getOwnPropertyDescriptor;var s=Object.getOwnPropertyNames;var b=Object.getPrototypeOf,l=Object.prototype.hasOwnProperty;var f=(n,e)=>()=>(e||n((e={exports:{}}).exports,e),e.exports),x=(n,e)=>{for(var t in e)o(n,t,{get:e[t],enumerable:!0})},a=(n,e,t,i)=>{if(e&&typeof e==\"object\"||typeof e==\"function\")for(let m of s(e))!l.call(n,m)&&m!==t&&o(n,m,{get:()=>e[m],enumerable:!(i=c(e,m))||i.enumerable});return n};var g=(n,e,t)=>(t=n!=null?v(b(n)):{},a(e||!n||!n.__esModule?o(t,\"default\",{value:n,enumerable:!0}):t,n)),_=n=>a(o({},\"__esModule\",{value:!0}),n);var u=f((M,d)=>{d.exports=_jsx_runtime});var h={};x(h,{default:()=>w});var r=g(u());function p(n){let{ComponentPreview:e}=n.components||{};return e||j(\"ComponentPreview\",!0),(0,r.jsxs)(r.Fragment,{children:[(0,r.jsx)(e,{name:\"bento-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"marquee-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"marquee-demo-vertical\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"animated-list-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"animated-beam-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"animated-beam-multiple-inputs\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"animated-beam-multiple-outputs\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"border-beam-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"border-beam-demo-2\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"border-beam-demo-3\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"border-beam-demo-4\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"shine-border-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"magic-card-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"dock-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"particles-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"blur-fade-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"blur-fade-text-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"retro-grid-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"orbiting-circles-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"ripple-demo\",preview:!0}),`\n`,(0,r.jsx)(e,{name:\"confetti-demo\",preview:!0})]})}function w(n={}){let{wrapper:e}=n.components||{};return e?(0,r.jsx)(e,{...n,children:(0,r.jsx)(p,{...n})}):p(n)}function j(n,e){throw new Error(\"Expected \"+(e?\"component\":\"object\")+\" `\"+n+\"` to be defined: you likely forgot to import, pass, or provide it.\")}return _(h);})();\n;return Component;"
+    }
+  },
+  {
+    "content": "",
+    "title": "Privacy",
+    "description": "The Privacy Policy for Magic UI",
+    "_meta": {
+      "filePath": "privacy.mdx",
+      "fileName": "privacy.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "privacy"
+    },
+    "slug": "/privacy",
+    "slugAsParams": "privacy",
+    "body": {
+      "raw": "",
+      "code": "var Component=(()=>{var _=Object.create;var a=Object.defineProperty;var f=Object.getOwnPropertyDescriptor;var d=Object.getOwnPropertyNames;var j=Object.getPrototypeOf,M=Object.prototype.hasOwnProperty;var g=(t,n)=>()=>(n||t((n={exports:{}}).exports,n),n.exports),l=(t,n)=>{for(var e in n)a(t,e,{get:n[e],enumerable:!0})},c=(t,n,e,u)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let o of d(n))!M.call(t,o)&&o!==e&&a(t,o,{get:()=>n[o],enumerable:!(u=f(n,o))||u.enumerable});return t};var p=(t,n,e)=>(e=t!=null?_(j(t)):{},c(n||!t||!t.__esModule?a(e,\"default\",{value:t,enumerable:!0}):e,t)),C=t=>c(a({},\"__esModule\",{value:!0}),t);var s=g((X,m)=>{m.exports=_jsx_runtime});var D={};l(D,{default:()=>i});var r=p(s());function x(t){return(0,r.jsx)(r.Fragment,{})}function i(t={}){let{wrapper:n}=t.components||{};return n?(0,r.jsx)(n,{...t,children:(0,r.jsx)(x,{...t})}):x(t)}return C(D);})();\n;return Component;"
+    }
+  },
+  {
+    "content": "",
+    "title": "Terms & Conditions",
+    "description": "Read our terms and conditions",
+    "_meta": {
+      "filePath": "terms.mdx",
+      "fileName": "terms.mdx",
+      "directory": ".",
+      "extension": "mdx",
+      "path": "terms"
+    },
+    "slug": "/terms",
+    "slugAsParams": "terms",
+    "body": {
+      "raw": "",
+      "code": "var Component=(()=>{var _=Object.create;var a=Object.defineProperty;var f=Object.getOwnPropertyDescriptor;var d=Object.getOwnPropertyNames;var j=Object.getPrototypeOf,M=Object.prototype.hasOwnProperty;var g=(t,n)=>()=>(n||t((n={exports:{}}).exports,n),n.exports),l=(t,n)=>{for(var e in n)a(t,e,{get:n[e],enumerable:!0})},c=(t,n,e,u)=>{if(n&&typeof n==\"object\"||typeof n==\"function\")for(let o of d(n))!M.call(t,o)&&o!==e&&a(t,o,{get:()=>n[o],enumerable:!(u=f(n,o))||u.enumerable});return t};var p=(t,n,e)=>(e=t!=null?_(j(t)):{},c(n||!t||!t.__esModule?a(e,\"default\",{value:t,enumerable:!0}):e,t)),C=t=>c(a({},\"__esModule\",{value:!0}),t);var s=g((X,m)=>{m.exports=_jsx_runtime});var D={};l(D,{default:()=>i});var r=p(s());function x(t){return(0,r.jsx)(r.Fragment,{})}function i(t={}){let{wrapper:n}=t.components||{};return n?(0,r.jsx)(n,{...t,children:(0,r.jsx)(x,{...t})}):x(t)}return C(D);})();\n;return Component;"
+    }
+  }
+];
