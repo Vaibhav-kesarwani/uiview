f7a3dbe381c6a8ea012e9bf67c3d318e04784d7c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-05-21 15:03:06 +0530

    Fix: fix the hero section

--- registry/example/tweet-card-meta-preview.tsx
+++ registry/example/tweet-card-meta-preview.tsx
@@ -1,5 +1,5 @@
 import { ClientTweetCard } from "@/registry/vectorui/client-tweet-card";
 
 export default function TweetMetaPreview() {
-  return <ClientTweetCard id="1675849118445436929" className="shadow-2xl" />;
+  return <ClientTweetCard id="1925121702536663443" className="shadow-2xl" />;
 }
