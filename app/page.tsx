0656ce0eda8b74042789ab42d8417f2967fe5b55
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-04 11:17:20 +0530

    Refactor the main code of the site to default state

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import { Header } from "@/components/header";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
-    <div className="flex flex-col min-h-screen">
-      <Header />
+    <div>
+      <Button>Click me</Button>
     </div>
-  );
+  )
 }
