988b4ed45e95283c44d2f66c973120419de427c7
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-07 16:53:36 +0530

    style: make the hero text in the site

---
+++ components/hero.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="flex flex-col items-center mt-40 h-screen px-4 text-center">
+      <h1 className="text-4xl sm:text-6xl lg:text-7xl font-bold max-w-5xl leading-none text-gray-200">
+        The Ultimate Library of Open-Source UI
+      </h1>
+    </div>
+  );
+};
+
+export default Hero;
