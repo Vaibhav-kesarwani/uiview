aacab89e255636db1e3bea17a991b7c518df009f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-07 12:24:10 +0530

    chore: move the search bar component into the navbarbutton for better use cases

--- components/navbar-buttons.tsx
+++ components/navbar-buttons.tsx
@@ -1,23 +1,14 @@
 import Link from "next/link";
 import React from "react";
-import { Button } from "./ui/button";
-import { PlusIcon, RocketIcon } from "lucide-react";
 import CreateButton from "./create-button";
+import SearchBar from "./search-bar";
 
 const NavbarButtons = () => {
   return (
-    <div className="flex items-center gap-2">
+    <div className="flex items-center gap-5">
+      <SearchBar />
       <Link href={"/login"}>
         <CreateButton />
-      </Link>
-      <Link href={"/create"}>
-        <Button
-          variant="outline"
-          className="bg-muted-foreground p-5 hidden md:inline-flex cursor-pointer text-muted-foreground"
-        >
-          <RocketIcon className="h-5 w-5" />
-          Join the Community
-        </Button>
       </Link>
     </div>
   );
