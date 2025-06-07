22c4dd66359ce1a51982fe8e0a8969b99d1d0f11
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-07 13:31:28 +0530

    fix: fix the FOUC(Flash of Unstyled Content) in the site

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,11 +1,19 @@
-"use client"
+"use client";
 
-import * as React from "react"
-import { ThemeProvider as NextThemesProvider } from "next-themes"
+import React, { useEffect, useState } from "react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
-export function ThemeProvider({
-  children,
-  ...props
-}: React.ComponentProps<typeof NextThemesProvider>) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: React.ComponentProps<typeof NextThemesProvider>) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    // Avoid rendering until mounted so no mismatch
+    return null; // or a loading spinner if you want
+  }
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
 }
