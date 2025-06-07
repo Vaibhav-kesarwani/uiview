a651903ca393e2fa4908162d096c5821768b82ee
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-07 11:22:37 +0530

    style: updated the style of the navbarbuttons in the navbar

--- components/navbar-buttons.tsx
+++ components/navbar-buttons.tsx
@@ -2,23 +2,23 @@ import Link from "next/link";
 import React from "react";
 import { Button } from "./ui/button";
 import { PlusIcon, RocketIcon } from "lucide-react";
+import CreateButton from "./create-button";
 
 const NavbarButtons = () => {
   return (
     <div className="flex items-center gap-2">
       <Link href={"/login"}>
-        <Button className="bg-blue-600 p-5 hidden md:inline-flex cursor-pointer hover:bg-blue-500 text-white">
-          <PlusIcon className="h-4 w-4" />
-          <p>Create</p>
+        <CreateButton />
+      </Link>
+      <Link href={"/create"}>
+        <Button
+          variant="outline"
+          className="bg-muted-foreground p-5 hidden md:inline-flex cursor-pointer text-muted-foreground"
+        >
+          <RocketIcon className="h-5 w-5" />
+          Join the Community
         </Button>
       </Link>
-      <Button
-        variant="outline"
-        className="bg-muted-foreground p-5 hidden md:inline-flex"
-      >
-        <RocketIcon className="h-4 w-4" />
-        <Link href="/signup">Join the Community</Link>
-      </Button>
     </div>
   );
 };
