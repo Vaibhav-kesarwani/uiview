d1d5f43bb06dd075140273d72af18e02611088b0
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-08 20:01:09 +0530

    chore: move the header and footer file into the layout for the better rendering

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import { Header } from "@/components/Header/header";
 import Hero from "@/components/Main/hero";
 
 export default function Home() {
   return (
     <>
-      <Header />
       <Hero />
-      {/* <Footer /> */}
     </>
   );
 }
