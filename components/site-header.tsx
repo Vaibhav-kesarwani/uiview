710081723382c0fe1e7eb65bbff3a81254df23d4
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-10 13:27:07 +0530

    feat: added the mobile navbar in the site

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,9 +1,11 @@
 import { MainNav } from "./main-nav";
+import { MobileNav } from "./mobile-nav";
 
 export function SiteHeader() {
   return (
     <div className="pl-8 pr-8 pt-2">
       <MainNav />
+      <MobileNav />
     </div>
   );
 }
