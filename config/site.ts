6b73671106fde2cf5b0aa99f889dc5670868ca66
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-10 18:39:59 +0530

    chore: update the navbar in mobile view and desktop view

---
+++ config/site.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  name: "Uiview",
+  url: "https://uiview.vercel.app/",
+  ogImage: "https://uiview.vercel.app/og",
+  description:
+    "Uiview is a modern UI component library offering reusable, scalable, and visually polished building blocks for fast, consistent interface development.",
+  links: {
+    twitter: "https://twitter.com/Vaibhav_k__",
+    github: "https://github.com/Vaibhav-kesarwani",
+    linkedin: "https://www.linkedin.com/in/vaibhavdev",
+  },
+  keywords: [
+    "React",
+    "Tailwind CSS",
+    "Motion",
+    "Landing Page",
+    "Components",
+    "Next.js",
+  ],
+};
+
+export type SiteConfig = typeof siteConfig;
