b49542332ed8cd7bfb50f8de7c6f87357cf6d2f6
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-10 20:00:05 +0530

    feat: make the top contributors marquee

---
+++ components/sections/top-creator.tsx
@@ -0,0 +1,10 @@
+import { MarqueeCard } from "../marquee-card";
+
+export default function TopCreator() {
+  return (
+    <div className="mt-30 mb-20">
+      <div className="text-3xl lg:text-5xl font-bold text-center my-8">Top Creators</div>
+      <MarqueeCard />
+    </div>
+  );
+}
