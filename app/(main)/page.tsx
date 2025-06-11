bd3080a0b04da99671bc14fa9ab722244adfa76f
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-11 15:32:20 +0530

    fix: fix the github star button

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,3 +1,4 @@
+import { GoldenGlow } from "@/components/golden-glow";
 import Feature from "@/components/sections/feature";
 import Hero from "@/components/sections/hero";
 import TopCreator from "@/components/sections/top-creator";
@@ -8,6 +9,9 @@ export default function Home() {
     <>
       <div className="hidden lg:flex">
         <Spotlight />
+      </div>
+      <div className="relative lg:hidden">
+        <GoldenGlow />
       </div>
       <Hero />
       <Feature />
