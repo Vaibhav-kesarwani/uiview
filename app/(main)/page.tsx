93c0ffa59f828da6dbd3718c0dd0abdff62adab8
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-11 09:49:42 +0530

    Made the spotlight animation for the website

--- app/(main)/page.tsx
+++ app/(main)/page.tsx
@@ -1,10 +1,12 @@
 import Feature from "@/components/sections/feature";
 import Hero from "@/components/sections/hero";
 import TopCreator from "@/components/sections/top-creator";
+import { Spotlight } from "@/components/ui/spotlight";
 
 export default function Home() {
   return (
     <>
+      <Spotlight />
       <Hero />
       <Feature />
       <TopCreator />
