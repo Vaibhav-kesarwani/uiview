c1192ae0c339678be457fefafa92dc78d0c4839c
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-16 23:28:36 +0530

    style: make the style fix in the code in the feature section

--- components/sections/feature.tsx
+++ components/sections/feature.tsx
@@ -2,7 +2,7 @@ import { FeatureCard } from "../feature-card";
 
 export default function Feature() {
   return (
-    <div className="mb-20 p-8 lg:p-0">
+    <div className="mt-20 mb-20 p-8 lg:p-0">
       <div className="text-3xl md:text-4xl lg:text-5xl font-bold text-center my-8">
         Standout Features
       </div>
