6e2d36684a9ffed555519707e67e7c9d61268779
Vaibhav-kesarwani <vaibhavkesarwani100@gmail.com>
2025-06-17 00:00:23 +0530

    fix: fix the styling of the top creator of the marquee mb -> 30

--- components/sections/top-creator.tsx
+++ components/sections/top-creator.tsx
@@ -2,7 +2,7 @@ import { MarqueeCard } from "../marquee-card";
 
 export default function TopCreator() {
   return (
-    <div className="mb-20">
+    <div className="mb-30">
       <div className="text-3xl md:text-4xl lg:text-5xl font-bold text-center my-8">
         Top Creators
       </div>
